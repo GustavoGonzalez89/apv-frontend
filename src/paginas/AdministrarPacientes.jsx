3b9e9db49b08e45aa536c2a054d3f41a6134676b
GustavoDanielGonzalez <82666168+GustavoDanielGonzalez@users.noreply.github.com>
2023-10-21 17:09:31 +0200

    Agregado nuevas cosas

--- src/paginas/AdministrarPacientes.jsx
+++ src/paginas/AdministrarPacientes.jsx
@@ -6,23 +6,23 @@ const AdministrarPacientes = () => {
   const [mostrarFormulario, setMostrarFormulario] = useState(false)
 
   return (
-  
-      <div className="flex flex-col md:flex-row">
-        <button
-          type="button"
-          className="bg-indigo-600 text-white font-bold text-sm mx-auto p-3 w-3/5 rounded-md uppercase mb-10 block md:hidden"
-          onClick={() => setMostrarFormulario(!mostrarFormulario) }
-        >{`${mostrarFormulario ? 'ocultar formulario': 'mostrar formulario'}`}
-        </button>
 
-        <div className={`${mostrarFormulario ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
-          <Formulario />
-        </div>
+    <div className="flex flex-col md:flex-row">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white font-bold text-sm mx-auto p-3 w-3/5 rounded-md uppercase mb-10 block md:hidden"
+        onClick={() => setMostrarFormulario(!mostrarFormulario)}
+      >{`${mostrarFormulario ? 'ocultar formulario' : 'mostrar formulario'}`}
+      </button>
 
-        <div className="md:w-1/2 lg:w-3/5">
-          <ListaPacientes />
-        </div>
+      <div className={`${mostrarFormulario ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
+        <Formulario />
       </div>
+
+      <div className="md:w-1/2 lg:w-3/5">
+        <ListaPacientes />
+      </div>
+    </div>
   )
 }
 
